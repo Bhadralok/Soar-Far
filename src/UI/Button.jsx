81ff9bbc47782ea75d52f74c92e95426a2c9abd4
Bhadralok <gr8prinzini338@gmail.com>
2025-08-09 11:07:26 +0100

    Modified

--- src/UI/Button.jsx
+++ src/UI/Button.jsx
@@ -3,13 +3,13 @@ import { MdArrowBack, MdArrowForward } from "react-icons/md";
 export default function Button({ children, isButton = true, type = "login" }) {
   const styles = {
     login:
-      "bg-white border-2 border-primary-blue text-[0.981rem] cursor-pointer rounded-2xl font-medium w-[6.506rem] h-[2.913rem] py-5 px-2 flex items-center justify-center",
+      "bg-white border-2 border-primary-blue outline-primary-blue hover:outline-3 text-[0.981rem] cursor-pointer rounded-2xl font-medium w-[6.506rem] h-[2.913rem] py-5 px-2 flex items-center justify-center",
     signUp:
-      "bg-primary-blue rounded-2xl text-[0.981rem] w-[6.506rem] h-[2.913rem] cursor-pointer py-5 px-2 font-medium flex items-center justify-center",
+      "bg-primary-blue outline-black hover:outline-3 rounded-2xl text-[0.981rem] w-[6.506rem] h-[2.913rem] cursor-pointer py-5 px-2 font-medium flex items-center justify-center",
     bookNow:
-      "bg-primary-blue rounded-2xl text-[0.981rem] w-[6.906rem] h-[2.913rem] cursor-pointer py-5 font-semibold flex items-center justify-center",
+      "bg-primary-blue rounded-2xl outline-black hover:outline-3 text-[0.981rem] w-[6.906rem] h-[2.913rem] cursor-pointer py-5 font-semibold flex items-center justify-center",
     getStarted:
-      "px-2 py-4 w-[14.563rem] rounded-2xl text-2xl font-semibold cursor-pointer h-[3.938rem] bg-primary-blue text-secondary-blue",
+      "px-2 py-4 w-[14.563rem] outline-black hover:outline-3 rounded-2xl text-2xl font-semibold cursor-pointer h-[3.938rem] bg-primary-blue text-secondary-blue",
   };
 
   return (
