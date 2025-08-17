1192db41a358fd4841adf0686e8d9e5fb273e734
Bhadralok <gr8prinzini338@gmail.com>
2025-08-17 03:35:55 +0100

    fixed some rounded corners in the buttons

--- src/UI/Button.jsx
+++ src/UI/Button.jsx
@@ -9,13 +9,13 @@ export default function Button({
 }) {
   const styles = {
     login:
-      "bg-white border-2 border-primary-blue outline-primary-blue hover:outline-3 text-[0.981rem] cursor-pointer rounded-2xl font-medium w-[6.506rem] h-[2.913rem] py-5 px-2 flex items-center justify-center",
+      "bg-white border-2 border-primary-blue outline-primary-blue hover:outline-3 text-[0.981rem] cursor-pointer rounded-full font-medium w-[6.506rem] h-[2.913rem] py-5 px-2 flex items-center justify-center",
     signUp:
-      "bg-primary-blue outline-black hover:outline-3 rounded-2xl text-[0.981rem] w-[6.506rem] h-[2.913rem] cursor-pointer py-5 px-2 font-medium flex items-center justify-center",
+      "bg-primary-blue outline-black hover:outline-3 rounded-full text-[0.981rem] w-[6.506rem] h-[2.913rem] cursor-pointer py-5 px-2 font-medium flex items-center justify-center",
     bookNow:
       "bg-primary-blue rounded-2xl outline-black hover:outline-3 text-[0.981rem] w-[6.906rem] h-[2.913rem] cursor-pointer py-5 font-semibold flex items-center justify-center",
     getStarted:
-      "px-2 py-4 w-[14.563rem] outline-black hover:outline-3 rounded-2xl text-2xl font-semibold cursor-pointer h-[3.938rem] bg-primary-blue text-secondary-blue",
+      "px-2 py-4 w-[14.563rem] outline-black hover:outline-3 rounded-full text-2xl font-semibold cursor-pointer h-[3.938rem] bg-primary-blue text-secondary-blue",
   };
 
   return (
